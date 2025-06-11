e39257cf07128fb5a7ddd3f57b5889e4f1ba0808
Teo_You <teoyou881@gmail.com>
2025-06-11 18:05:23 -0400

    impl AppLayout.tsx

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import {Button, Container, Form, Nav, Navbar} from 'react-bootstrap';
+import {Outlet} from 'react-router-dom';
 
 const AppLayout = () => {
   return (
-      <>AppLayout</>
-  )
+      <>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container fluid>
+            <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll"/>
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                  className="me-auto my-2 my-lg-0"
+                  style={{maxHeight:'100px'}}
+                  navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Outlet/>
+      </>
+  );
 }
+
 export default AppLayout
