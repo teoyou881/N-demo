9283c50f7143bb593742107e9bc73d74d4a0bb0c
Teo_You <teoyou881@gmail.com>
2025-06-11 19:46:50 -0400

    set react-query to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { createRoot } from 'react-dom/client'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import App from './App.tsx'
 
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
-
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>,
+    </QueryClientProvider>,
 )
